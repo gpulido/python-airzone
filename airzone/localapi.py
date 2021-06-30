1269bad28a4d471f7ed8c589fdff0c9d280f371b
Stefan <86342051+Mike-de-bike@users.noreply.github.com>
2021-06-30 23:36:25 +0200

    Localapi 30.06.2021 (#11)
    * fixes to ini of machine
    
    * fixing typos etc.
    
    * fixes in zone methods
    reduction exception handling in machine methods
    
    * get and set global system mode
    
    sample output:
    
    Number of zones:  5
    Machine with id: 1
    Zones:
    Modes: {1: 'Stop', 2: 'Cooling', 3: 'Heating', 4: 'Fan', 5: 'Dry'}
    System mode: Stop
    Zones
    Zone with id: 1 Name: Titus Zone is On: 0 Room Temp: 23.7 Room humidity: 71 Max_Temp: 30 Min_Temp: 15
    Zone with id: 2 Name: Eltern Zone is On: 0 Room Temp: 23.8 Room humidity: 68 Max_Temp: 30 Min_Temp: 15
    Zone with id: 3 Name: Thea Zone is On: 0 Room Temp: 24.3 Room humidity: 65 Max_Temp: 30 Min_Temp: 15
    Zone with id: 4 Name: Ella Zone is On: 0 Room Temp: 24.1 Room humidity: 66 Max_Temp: 30 Min_Temp: 15
    Zone with id: 5 Name: Buero Zone is On: 0 Room Temp: 24.1 Room humidity: 64 Max_Temp: 30 Min_Temp: 15
    
    * air-demand
    units
    floor-demand
    
    Co-authored-by: Gabriel Pulido <gabriel.pulidodetorres@gmail.com>

--- airzone/localapi.py
+++ airzone/localapi.py
@@ -6,16 +6,20 @@ import requests  # type: ignore
 class Machine:
 
     def __init__(self, machine_ipaddr, port=3000, system_id=1):
-        self._machine_ip = machine_ipaddr
-        self._machine_id = system_id
-        self._machine_state = None
         self._API_ENDPOINT = f"http://{machine_ipaddr}:{str(port)}/api/v1/hvac"
+        self._machine_id = system_id
+        self._machine_ip = machine_ipaddr
         self._data = {'SystemID': self._machine_id, 'ZoneID': 0}
+        self._error_log = []
         self._machine_state = None
         self._zones = {}
-        self.get_system_data()        
-        self._system_modes = self.get_system_modes()
-
+        self._system_modes = {1: "Stop", 2: "Cooling", 3: "Heating", 4: "Fan", 5: "Dry"}
+        # fan speed is not in use
+        self._fan_speeds = {0: "Auto", 1: "Low speed", 2: "Medium Speed", 3: "High Speed"}
+        self._units = {0: "Celsius", 1: "Fahrenheit"}        
+        self.get_system_data()
+        self.discover_zones()
+        
     
     @property
     def machine_state(self):
@@ -40,28 +44,17 @@ class Machine:
     
     def get_system_data(self):
         try:
-            response = requests.post(url=self._API_ENDPOINT,
-                                     json=self._data)
-            if response.status_code == 200:
-                response_json = response.json()
-                self._machine_state = response_json['data']
-            elif response.status_code >= 500:
-                print(f'[!] [{response.status_code}] Server Error')
+            self._response = requests.post(url=self._API_ENDPOINT,
+                                           json=self._data)
+            if self._response.status_code == 200:
+                self._response_json = self._response.json()
+                self._machine_state = self._response_json['data']
+            elif self._response.status_code >= 500:
+                print(f'[!] [{self._response.status_code}] Server Error')
                 return None
-            response.raise_for_status()
-        except requests.exceptions.HTTPError as e:
+            self._response.raise_for_status()
+        except requests.exceptions.RequestException as e:
             raise SystemExit(e)
-        except requests.exceptions.Timeout as e:
-            raise SystemExit(e)
-        except requests.exceptions.ConnectionError as e:
-            print("Error Connecting:", e)
-        except requests.exceptions.RequestException as e:  # This is the correct syntax
-            raise SystemExit(e)
-
-    def get_system_modes(self):
-        for listmember in self._machine_state:
-            if 'modes' in listmember:
-                return listmember['modes']
 
     def get_zone_property(self, zone_id, prop):
         for listmember in self._machine_state:
@@ -70,36 +63,31 @@ class Machine:
 
     def set_zone_parameter_value(self, zone_id, parameter, value):
         try:
-            # print(self._data)
             self._data['ZoneID'] = zone_id
             self._data[parameter] = value
-            # print(self._data)
             response = requests.put(url=self._API_ENDPOINT,
                                     json=self._data)
             if response.status_code == 200:
-                # TODO: this response is the same as if we ask for the 0 zone?
                 response_json = response.json()
                 self.machine_state = response_json['data']
             elif response.status_code >= 500:
-                print(f'[!] [{response.status_code}] Server Error')
+                print(f'[!] [{response.status_code}] Server Error')                
                 return None
             self._response.raise_for_status()
-        except requests.exceptions.HTTPError as e:
-            raise SystemExit(e)
-        except requests.exceptions.Timeout as e:
-            raise SystemExit(e)
-        except requests.exceptions.ConnectionError as e:
-            print("Error Connecting:", e)
-        except requests.exceptions.RequestException as e:  # This is the correct syntax
+        except requests.exceptions.RequestException as e:
             raise SystemExit(e)
 
-    def set_mode(self, mode):
-        self._set_zone_parameter_value(0, 'mode', mode)
+    def set_fan_speed(self, mode):
+        self.set_zone_parameter_value(0, 'speed', mode)
+
+    def set_system_mode(self, mode):
+        self.set_zone_parameter_value(0, 'mode', mode)
+
+    def get_system_modes(self):
+        return self._system_modes
 
     def get_mode(self):
-        # Mode is global to the system TODO: check that the zone_id = 0 is available for this
-        # TODO: Transform in enum
-        return self.get_zone_property(0, 'mode')
+        return self.get_zone_property(1, 'mode')
 
     def unique_id(self):
         return f'Local_Api{self._machine_id}_{str(self._machine_ip)}'
@@ -107,7 +95,9 @@ class Machine:
     def __str__(self):
         zs = "\n".join([str(z) for z in self.get_zones()])
         return "Machine with id: " + str(self._machine_id) + \
-               "Zones: \n" + zs
+               "\nZones: \nModes: " + str(self._system_modes) + \
+               "\nSystem mode: " + str(self._system_modes[self.get_mode()]) + \
+               "\nZones\n" + zs
 
 
 class Zone:
@@ -133,22 +123,22 @@ class Zone:
         self.set_parameter_value('on', 0)
 
     def toggle_mode(self):
-        if self.get_operation_mode() == 1:
+        if self._machine.get_operation_mode() == 1:
             self.set_parameter_value('on', 0)
         else:
             self.set_parameter_value('on', 1)
 
     def set_setpoint(self, setpoint):
-        self.set_zone_parameter_value('setpoint', setpoint)
+        self._machine.set_zone_parameter_value('setpoint', setpoint)
 
     def set_maxtemp(self, maxtemp):
-        self.set_zone_parameter_value('maxTemp', maxtemp)
+        self._machine.set_zone_parameter_value('maxTemp', maxtemp)
 
     def set_mintemp(self, mintemp):
-        self.set_zone_parameter_value('minTemp', mintemp)
+        self._machine.set_zone_parameter_value('minTemp', mintemp)
 
     def set_name(self, name):
-        self.set_zone_parameter_value('name', name)
+        self._machine.set_zone_parameter_value('name', name)
 
     def get_name(self):
         return self.get_property('name')
@@ -171,11 +161,22 @@ class Zone:
     def get_room_humidity(self):
         return self.get_property('humidity')
 
+    def get_air_demand(self):
+        return self.get_property('air_demand')
+
+    def get_floor_demand(self):
+        return self.get_property('floor_demand')
+
+    def get_units(self):
+        return self.get_property('units')
+
     def __str__(self):
         return "Zone with id: " + str(self._zone_id) + \
                " Name: " + str(self.get_name()) + \
                " Zone is On: " + str(self.is_on()) + \
-               " Room Temp: " + str(self.get_room_temp()) + \
+               " Air demand is: " + str(self.get_air_demand()) + \
+               " Room Temp: " + str(round(self.get_room_temp(), 1)) + \
+               " " + str(self._machine._units[self.get_units()]) +\
                " Room humidity: " + str(self.get_room_humidity()) + \
                " Max_Temp: " + str(self.get_max_temp()) + \
                " Min_Temp: " + str(self.get_min_temp())
