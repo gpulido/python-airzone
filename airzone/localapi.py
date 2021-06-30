25f90e68b6305478c975aa752c7f0d372e22805b
gpulido <gabriel.pulidodetorres@gmail.com>
2021-06-30 21:15:29 +0200

    refactor to manage state update

--- airzone/localapi.py
+++ airzone/localapi.py
@@ -1,3 +1,5 @@
+""" Airzone Local api integration
+"""
 import requests  # type: ignore
 
 
@@ -10,18 +12,32 @@ class Machine:
         self._API_ENDPOINT = f"http://{machine_ipaddr}:{str(port)}/api/v1/hvac"
         self._data = {'SystemID': self._machine_id, 'ZoneID': 0}
         self._machine_state = None
-        self._zones = []
-        self.get_system_data()
-        self.discover_zones()
+        self._zones = {}
+        self.get_system_data()        
         self._system_modes = self.get_system_modes()
 
-    def discover_zones(self):
-        self._zones = [Zone(self, listmember['zoneID']) for listmember in self._machine_state if
-                       listmember['zoneID'] != 0]
+    
+    @property
+    def machine_state(self):
+        return self._machine_state
+
+    @machine_state.setter
+    def machine_state(self, value):
+        self._machine_state = value
+        self.update_zones()
+
+    def discover_zones(self):                
+        self._zones = {z['zoneID']: Zone(self, z['zoneID'], z) for z in self._machine_state if z['zoneID'] != 0}
+
+    def update_zones(self):
+        if self._zones == {}:
+            self.discover_zones()
+        for z in self._machine_state:
+            self._zones[z['zoneID']].set_zone_state(z)
 
     def get_zones(self):
-        return self._zones
-
+        return self._zones.values()
+    
     def get_system_data(self):
         try:
             response = requests.post(url=self._API_ENDPOINT,
@@ -63,7 +79,7 @@ class Machine:
             if response.status_code == 200:
                 # TODO: this response is the same as if we ask for the 0 zone?
                 response_json = response.json()
-                self._machine_state = response_json['data']
+                self.machine_state = response_json['data']
             elif response.status_code >= 500:
                 print(f'[!] [{response.status_code}] Server Error')
                 return None
@@ -87,10 +103,7 @@ class Machine:
 
     def unique_id(self):
         return f'Local_Api{self._machine_id}_{str(self._machine_ip)}'
-
-    def get_machine_state(self):
-        return self._machine_state
-
+  
     def __str__(self):
         zs = "\n".join([str(z) for z in self.get_zones()])
         return "Machine with id: " + str(self._machine_id) + \
@@ -98,12 +111,17 @@ class Machine:
 
 
 class Zone:
-    def __init__(self, machine, zone_id):
+    def __init__(self, machine, zone_id, data = {}):
         self._machine = machine
-        self._zone_id = zone_id
+        self._zone_id = zone_id        
+        self.set_zone_state(data)        
+    
+    def set_zone_state(self, state):
+        self._zone_state = state
+
 
     def get_property(self, prop):
-        return self._machine.get_zone_property(self._zone_id, prop)
+        return self._zone_state[prop]        
 
     def set_parameter_value(self, prop, value):
         self._machine.set_zone_parameter_value(self._zone_id, prop, value)
@@ -167,7 +185,6 @@ if __name__ == '__main__':
     # Lines for Tests. Adapt argument ip address and system id (1 == standard).
     m = Machine('192.168.90.9', system_id=1)
     print("Printing Post JSON data")
-    print(m.get_machine_state())
+    print(m.machine_state)
     print("Number of zones: ", len(m.get_zones()))
     print(m)
-
